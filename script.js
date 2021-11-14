af925ca4d621eed4f956003cff0bc8e173b49388
emorilebo <emorylebo@gmail.com>
2021-11-14 09:33:20 +0100

    display comparism to screen

--- script.js
+++ script.js
@@ -5,13 +5,14 @@
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses
+let guesses = [];
 
 // Variable for store the correct random number
 let correctNumber = getRandomNumber();
@@ -29,6 +30,8 @@ function playGame() {
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById("number-guess").value;
   displayResult(numberGuess);
+  saveGuessHistory(numberGuess);
+  displayHistory();
 }
 
 /**
@@ -78,6 +81,8 @@ function getRandomNumber() {
  */
 function saveGuessHistory(guess) {
   // *CODE GOES BELOW HERE *
+  guesses.push(guess);
+  console.log(guesses);
 }
 
 /**
@@ -89,10 +94,23 @@ function saveGuessHistory(guess) {
  * HINT: use while loop and string concatentation to create a list of guesses
  */
 function displayHistory() {
-  let index; // TODO
+  let index = guesses.length - 1; // TODO
   let list = "<ul class='list-group'>";
   // *CODE GOES BELOW HERE *
+  // for (index; index >= 0; index--) {
+  //   list +=
+  //     "<li class='list-group-item'>" + "You guessed " + guesses[index] + "<li>";
+  // }
+  while (index >= 0) {
+    list +=
+      "<li class='list-group-item'>" +
+      "You guessed " +
+      guesses[index] +
+      "</li>";
+    index--;
+  }
   list += "</ul>";
+  console.log(guesses);
   document.getElementById("history").innerHTML = list;
 }
 
